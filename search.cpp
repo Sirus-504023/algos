03b9fd7c222da4f83211e6bc569010ee7ebfa59b
Sirus-504023 <Sirus-504023@users.noreply.github.com>
2017-02-09 16:00:14 -0400

    search

--- search.cpp
+++ search.cpp
@@ -14,7 +14,16 @@ using namespace std;
 */
 int linearSearch(auto data, auto key);//prototype
 
-
+int linearSearch(auto data, auto key)
+{
+	for(int i =0; i <data.size(); i ++)
+		{		
+		if (data[i] == key)
+			{ 
+			 return i;	
+	return -1;	}
+		}
+}
 int main()
 {
   vector<string> inputs;
